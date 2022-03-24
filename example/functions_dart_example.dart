520a918224e75e5d27b2f698add70592c01d70f5
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2022-03-24 23:04:24 +0900

    chore: initial commit

---
+++ example/functions_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:functions_dart/functions_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/functions_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library functions_dart;
+
+export 'src/functions_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
