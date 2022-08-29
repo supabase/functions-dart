b766e4a38470c759d8986709cb5817433729a384
PointSource <47544779+point-source@users.noreply.github.com>
2022-08-29 16:45:43 -0700

    Support null or empty body in json response

--- lib/src/functions_client.dart
+++ lib/src/functions_client.dart
@@ -51,7 +51,8 @@ class FunctionsClient {
 
     final dynamic data;
     if (responseType == ResponseType.json) {
-      data = await compute(json.decode, response.body);
+      final body = response.body;
+      data = body?.isEmpty ?? true ? null : await compute(json.decode, response.body);
     } else if (responseType == ResponseType.blob) {
       data = response.bodyBytes;
     } else if (responseType == ResponseType.arraybuffer) {
